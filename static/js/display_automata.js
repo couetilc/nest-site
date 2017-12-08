382e030336e47899385e8f683f66b0a1ab85798c
Connor Couetil <couetilc@gmail.com>
2017-12-08 15:52:14 -0500

    Created testing framework for 1d8bit Automata Library.  Updated script.

--- static/js/display_automata.js
+++ static/js/display_automata.js
@@ -9,120 +9,62 @@
  */
 
 
-var surfaces = {};
-const CELL_PARAM = { dim1 : 5, dim2 : 5 };
+var Automata = {};
 
 
-/******* Surface Functions                                      *******/
-
-var calcSurfaceDim = (ws, cp) => {
+var calcSurfaceDim = function (es, cp) {
     return {
         // Number of cells that fit in a generation given the window state
-        sizegen : Math.floor(ws.docWidth / cp.dim1),
+        sizegen : Math.floor(es.elemWidth / cp.dim1),
         // Number of generations that fit in the given window state.
-        numgen : Math.floor(ws.docHeight / cp.dim2)
+        numgen : Math.floor(es.elemHeight / cp.dim2)
     }
 };
 
 
-var instantiateSurface = (lib, dim, rule, elem, seed) => {
+var makeAutomata = function (lib, elem, rule, cparam, seed) {
+    state = ElementState(elem);
+    sparam = calcSurfaceDim(state, cparam);
+    ruleset = lib.generateRuleset(rule);
+
     return {
-        sf : lib.generateSurface(lib.generateRuleset(rule), dim, seed),
-        lib : lib,
-        elem : elem
-    }
+        lib: lib,
+        elem: elem,
+        state: ElementState(elem),
+        ruleset: ruleset,
+        cparam: cparam,
+        sparam: sparam,
+        sf: lib.generateSurface(ruleset, sparam, seed)
+    };
 };
 
 
-/******* DOM Manipulation Functions                             *******/
+var resizeAutomata = function (mata) {
+    mata.state = ElementState(mata.elem);
+    mata.sparam = calcSurfaceDim(mata.state, mata.cparam);
+    mata.sf = mata.lib.resizeSurface(mata.sf, mata.sparam);
 
-var WindowState = () => { 
+    return mata;
+}
+
+
+var ElementState = function (elem) { 
     return {
-        windowHeight : $(window).height(),
-        windowWidth : $(window).width(),
-        docHeight : $(document).height(),
-        docWidth : $(document).width()
+        elemHeight : elem.clientHeight,
+        elemWidth : elem.clientWidth
     }
 };
 
 
-var mutateDOMSurface = (sf, ws) => {
+function mutateDOMState(mata) {
     //TODO
-    //e.g. $('#' + id)
-};
+    //grab the elem within the surface and make it match the parameters
+    return;
+}
 
 
-/******* Cellular Automata Libraries                            *******/
-/*** (each manipulates a different type of Automata)                ***/
-
-/* Library of 1-dimensional 3-bit Cellular Automata Functions (
- * TODO - THOUGHTS
- *      : Integrate some Trie structure to speed up string lookup and/or
- *          computation
- *
- * What is a cell?
- *  A cell has one of two states, alive or dead, represented by a binary digit
- *  of 1 or 0.
- *
- * What is a generation?
- *  A collection of cells.  In this implementation, a generation will be
- *  represented by a string of bits, whose bit values define the state of a
- *  cell.
- *
- * What is a surface?
- *  An ordered collection of generations.  Each generation should be of the
- *  same size. In this implementation, a surface is an Array of strings, each
- *  of which is one generation.
- *
- * What is a cellular automaton?
- *  A cellular automaton is a grid of cells that change state over
- *  time according to some rule defined by its neighbors.  In this
- *  implementation the grid is a single generation on the surface.  Its cells'
- *  neighbors are the previous generations cells at the index positions left
- *  of, equal to, and right of the current cell's index.
- *
- * An example using strings as the storage value for each generation
- *  surface:   c0  c1  c2  c3
- *  e.g.    g0  0   0   1   1       Generation Format = String of length 4
- *          g1  A   B   C   D       Neighbor Format = (left, center, right)
- *
- *          surface = [g0, g1]
- *          Format of Generation #0 (g0) = "0011"
- *          Generation Size = 4
- *          Number of Generations = 2
- *
- *          A's neighbors = (g0c3, g0c0, g0c1)
- *          B's neighbors = (g0c0, g0c1, g0c2)
- *          C's neighbors = (g0c1, g0c2, g0c3)
- *          D's neighbors = (g0c2, g0c3, g0c0)
- *
- *          A's value = ruleset(A's neighbors)
- *          B's value = ruleset(B's neighbors)
- *          C's value = ruleset(C's neighbors)
- *          D's value = ruleset(D's neighbors)
- */
-
-const AutomataLib1D8bit = {
-    /* Return Value:
-     *      A surface, which is a rectangular array of strings representing
-     *      the cell states of its generations.
-     *
-     * Parameters:
-     *      ruleset:    Array with string keys of size 3.  The keys are a
-     *                  bitstring mapping to the value of the binary digits 
-     *                  of an 8-bit number, called the "rule".  Here, keys are
-     *                  the states of a cell's neighbors, and the
-     *                  corresponding value is the rule's digit at the key's 
-     *                  numeral. e.g. For rule=30, "010" -> "1" & "000" -> "0"
-     *       param:     Object with properties sizegen and numgen.
-     *                  sizegen = number of cells in each generation.
-     *                  numgen = number of generations in the surface.
-     *       seed:      Optional string that acts as the first generation for
-     *                  the generated surface.  Defaults to a string the size
-     *                  of param.sizegen filled with "0" except for a single
-     *                  centered "1" (left-bias e.g. length=4 -> seed="0100").
-     */
-    generateSurface : (ruleset, param, seed) => {
+var AutomataLib1D8bit = {
+    generateSurface : function (ruleset, param, seed) {
         let surface = null;
         //If parameter is not well initialized, nothing to generate.
         if (!param || param.numgen < 0 || param.sizegen < 0) {
@@ -137,21 +79,18 @@ const AutomataLib1D8bit = {
             seed = front + "1" + back;
         }
 
-        //Starting with the seed generation, new generations are calculated
-        //until all generations have been generated.
         surface = new Array(param.numgen);
         surface[0] = seed;
         for (let i = 1; i < param.numgen; i++) {
-            surface[i] = this.birthGeneration(ruleset
-                                             , surface[i - 1]
-                                             , param.sizegen
-                                             );
+            surface[i] = this.birthGeneration(ruleset,
+                                              surface[i - 1],
+                                              param.sizegen);
         }
 
         return surface;
     },
 
-    birthGeneration : (ruleset, oldgen, sizegen) => {
+    birthGeneration : function (ruleset, oldgen, sizegen) {
         let hood, newgen = "";       
         let mod = (val, div) => val - div * Math.floor(val / div);
 
@@ -165,28 +104,7 @@ const AutomataLib1D8bit = {
         return newgen;
     },
     
-    /*Resizes the cellular automata by either truncating width/length in the 
-     * event of a resize smaller operation, or by wrapping the width/length 
-     * in the event of a resize larger operation.  Returns the resized surface
-     *
-     * e.g. initial surface: nGen = 4, sizeGen = 4
-     *                       A B C D
-     *                       E F G H
-     *                       I J K L
-     *                       M N O P
-     *
-     *      resize smaller: nGen=2, sizeGen=3
-     *                       A B C                             
-     *                       E F G                             
-     *      resize larger: nGen=6, sizeGen=10
-     *                       A B C D A B C D A B
-     *                       E F G H E F G H E F
-     *                       I J K L I J K L I J
-     *                       M N O P M N O P M N
-     *                       A B C D A B C D A B
-     *                       E F G H E F G H E F
-     */
-    resizeGeneration : (surface, param) => {
+    resizeSurface : function (surface, param) {
         let newgen;
         let newsize = param.sizegen;
         let oldsize = surface.sf[0].length;
@@ -196,7 +114,7 @@ const AutomataLib1D8bit = {
         for (let gen = 0; gen < param.numgen; gen++) {
             let makeChunks = (size, maxchunk) => {
                 let quot = Math.floor(size / maxchunk);
-                let rem = size - maxchunk * Math.floor(size / maxchunk)
+                let rem = size - maxchunk * Math.floor(size / maxchunk);
 
                 let chunks = [...Array(quot)].map(() => maxsize);
                 chunks.append[rem];
@@ -208,7 +126,7 @@ const AutomataLib1D8bit = {
                 newgen += surface[gen].slice(0, chunk)
             }
 
-            //ensure heterogenous generations
+            //no homogeneous generations
             if (newgen.indexOf("0") === -1 || newgen.indexOf("1") === -1) {
                 let halveNround = (round, val) => round((val - 1) / 2);
                 let front = "0".repeat(halveNround(Math.floor, newsize));
@@ -220,7 +138,6 @@ const AutomataLib1D8bit = {
             newsf[gen] = newgen;
         }
 
-        //wrap new generations
         for (let gen = oldsize; i < newsize; gen++) {
             newsf[gen] = newsf[gen % oldsize];
         }
@@ -228,7 +145,7 @@ const AutomataLib1D8bit = {
         return newsf;
     },
 
-    generateRuleset : (rule) => {
+    generateRuleset : function (rule) {
         let rules = [];
         
         for (let pos = 0; pos < 8; pos++) {
@@ -239,33 +156,31 @@ const AutomataLib1D8bit = {
 
             rules[ pad(pos.toString(2), 3) ] = state.toString(2);
         }
+
+        return rules;
     }
 };
 
 
-/******* Initialized Surfaces                                   *******/
+document.addEventListener("DOMContentLoaded", (event) => {
+    Automata.background = makeAutomata(AutomataLib1D8bit,
+                                       document.getElementById('bg-surface'),
+                                       30,
+                                       { dim1 : 5, dim2 : 5 },
+                                       null);
+    for (let p in Automata) {
+        if (Automata.hasOwnProperty(p)) {
+            mutateDOMState(Automata[p]);
+        }
+    }
+});
 
-surfaces.background = instantiateSurface(
-    AutomataSurface1D8bit
-    , calcSurfaceDim( WindowState() , CELL_PARAM)
-    , rule = 30
-    , document.getElementById('bg-surface')
-    , seed = null
-    );
 
-
-/******* Event Triggers                                         *******/
-
-$(document).ready(
-    surfaces.map( sf => mutateDOMSurface(sf, WindowState()) )
-);
-    
-
-$(window).on('resize',
-    surfaces.map( sf => mutateDOMSurface(sf, WindowState()) )
-    //consider: 
-    //  surfaces.map( (sf, ws = WindowState()) => mutateDOMSurface(
-    //      sf.lib.resizeGeneration(sf, calcSurfaceDim(ws, CELL_PARAM))
-    //      , ws)
-    //  )
-);
+document.addEventListener('resize', (event) => {
+    for (let p in Automata) {
+        if (Automata.hasOwnProperty(p)) {
+            Automata[p] = resizeAutomata(Automata[p]);
+            mutateDOMState(Automata[p]);
+        }
+    }
+});
